bba516ef915e75dd072448fc818fe8f9fa14f0ed
AlexanderVerse <alexander.rangel@alumno.buap.mx>
2021-04-27 20:46:15 -0500

    :sparkles:Agregar mensaje de calificación
    Para mostrar los aciertos que tiene el usuario en el cuestionario a través de un AlertDialog

--- app/src/main/java/com/example/makequiz/GlobalData.kt
+++ app/src/main/java/com/example/makequiz/GlobalData.kt
@@ -1,12 +1,27 @@
 package com.example.makequiz
 
+import android.app.Activity
+import android.content.DialogInterface
+import androidx.appcompat.app.AlertDialog
+
 class GlobalData {
 
     var urlServer: String = "http://localhost:8080/"
     var urlSaveData: String = "http://localhost:8080/add"
 
-    fun showMessage(correctAnswer: Int, totalQuestion: Int)
+    fun showMessage(correctAnswer: Int, totalQuestion: Int, activity: Activity)
     {
-        
+        System.out.println("-----> Estoy dentro de SHOWMESSAGE")
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle("El quiz ha finalizado")
+        builder.setMessage("Tuviste $correctAnswer respuestas correctas  de $totalQuestion")
+            .setPositiveButton(
+                "Ok"
+            ) { dialog: DialogInterface?, id: Int ->
+                
+            }.setOnDismissListener { dialogInterface: DialogInterface? ->
+                
+            }
+        builder.create().show()
     }
 }
--- app/src/main/java/com/example/makequiz/TakeQuiz.kt
+++ app/src/main/java/com/example/makequiz/TakeQuiz.kt
@@ -1,5 +1,7 @@
 package com.example.makequiz
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +9,8 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import java.lang.reflect.Array
 
@@ -18,7 +22,7 @@ class TakeQuiz : AppCompatActivity()
     private val TAG_FRAGMENT = "TAG_FRAGMENT"
     private var arrayQuestion = emptyArray<String>()
     private var arrayAnswer = emptyArray<String>()
-    private var arrayAnswerUser = emptyArray<String>()
+    private var arrayAnswerUser  = emptyArray<String>()
     private var question: String? = null
     private var answer: String? = null
     private var totalQuestion = 0
@@ -43,6 +47,7 @@ class TakeQuiz : AppCompatActivity()
         totalQuestion = question?.split(",")?.size!!
         System.out.println("--->Total: $totalQuestion")
         questionText?.text = question!!.split(",")[indexQuestion]
+        arrayAnswerUser = kotlin.Array(totalQuestion) { indice -> (indice + 1).toString() }
     }
 
     fun nextQuestion(view: View)
@@ -59,10 +64,12 @@ class TakeQuiz : AppCompatActivity()
         {
             checkQuestion()
         }
+        answerText?.setText("")
     }
 
-    fun checkQuestion()
+    private fun checkQuestion()
     {
+        Toast.makeText(this, "Entre a verificar las respuestas ${arrayAnswerUser.size}", Toast.LENGTH_LONG).show()
         indexQuestion = 0
         var answerCorrect = 0
         for(element in arrayAnswerUser)
@@ -71,8 +78,31 @@ class TakeQuiz : AppCompatActivity()
             {
                 ++answerCorrect
             }
+            ++indexQuestion
         }
-        globalData?.showMessage(answerCorrect, totalQuestion)
+        answerCorrect = 0
+        showMessage(answerCorrect, totalQuestion)
+    }
+
+    private fun showMessage(correctAnswer: Int, totalQuestion: Int)
+    {
+        System.out.println("-----> Estoy dentro de SHOWMESSAGE")
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("El quiz ha finalizado")
+        builder.setMessage("Tuviste $correctAnswer respuestas correctas de $totalQuestion")
+            .setPositiveButton(
+                "Ok"
+            ) { dialog: DialogInterface?, id: Int ->
+
+            }.setOnDismissListener { dialogInterface: DialogInterface? ->
+
+            }
+        builder.create().show()
+        val intent: Intent = Intent(this, MainActivity::class.java).apply {
+
+        }
+        finish()
+        startActivity(intent)
     }
 
 }
